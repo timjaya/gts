9a0e54fb2b32988f9f3d31d6069d4b31342c94bf
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2020-01-08 10:53:38 -0600

    Add ts_prep()

---
+++ R/prep.R
@@ -0,0 +1,27 @@
+#' Convert tsibble to forecast ready object
+#'
+#' @param .ts A tsibble
+#'
+#' @return A nested data frame
+#' @export
+#'
+#' @examples
+#' your_ts %>%
+#'   ts_prep()
+ts_prep <- function(.ts) {
+
+  if(!is_tsibble(.ts)) abort(".ts must be a tsibble")
+
+  if (length(key_vars(.ts)) == 0) {
+    .ts %>%
+      nest() %>%
+      mutate(ts = data %>% map(as.ts)) %>%
+      select(-data)
+  } else {
+    .ts %>%
+      group_by_key() %>%
+      nest() %>%
+      mutate(ts = data %>% map(as.ts)) %>%
+      select(-data)
+  }
+}
