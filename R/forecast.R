f976f17b2a79923a8553b5731cc9ac5ff06c5b40
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2020-01-08 15:25:09 -0600

    Fix ts_forecast()

--- R/forecast.R
+++ R/forecast.R
@@ -30,51 +30,64 @@ ts_forecast <- function(.ts, h = NULL) {
   if (is.null(h)) h <- ts_frequency
   if (length(h) > 1) abort("h must be a single number")
 
-  time_class_fn <- if (ts_frequency == 52) {
-    tsibble::yearweek
-  } else if (ts_frequency == 12) {
+  time_class_fn <- if (ts_frequency == 12) {
     tsibble::yearmonth
   } else if (ts_frequency == 4) {
     tsibble::yearquarter
   }
 
-  if (ts_frequency == 1) abort("time-series must be weekly, quarterly, or monthly")
+  if (ts_frequency != 12 | ts_frequency != 4)
+    abort("time-series must be monthly or quarterly")
 
-  .ts %>%
+  forecast_df <- .ts %>%
     # Forecast time-series
     mutate_at(vars(ends_with("model")),
               list(forecast = ~map(.x, forecast::forecast, h = h))) %>%
     # Convert time-series to tibble
     mutate_at(vars(ends_with("forecast")),
               ~.x %>% map(~as_tibble(.x, rownames = "index") %>%
-                            clean_names() %>%
-                            mutate(index = ifelse(ts_frequency == 12,
-                                                  str_c(str_sub(index, 5, 8),
-                                                        str_sub(index, 1, 3),
-                                                        sep = " ") %>%
-                                                    time_class_fn(),
-                                                  time_class_fn(index)))
-                          )) %>%
+                            clean_names()
+              )) %>%
     # Remove "_model_" from names of forecasts
-    rename_all(str_replace, "_model_", "_") %>%
-    # Back-transform any log forecasts
-    mutate_at(vars(ends_with("log_forecast")),
-              ~map(.x, ~.x %>% mutate(point_forecast = exp(point_forecast)))) %>%
-    # Remove unnecessary columns
-    select(group_cols(), ends_with("model"), ends_with("forecast")) %>%
-    # Overwrite model objects with 1 to reduce size of data
-    mutate_at(vars(ends_with("model")), ~1) %>%
-    pivot_longer(ends_with("model"), names_to = "model") %>%
-    select(-value) %>%
-    pivot_longer(ends_with("forecast"), values_to = "forecast") %>%
-    # Remove duplicate column labeling models
-    select(-name) %>%
-    # Group by all variables that aren't forecasts
-    group_by_at(vars(-ends_with("forecast"))) %>%
-    select(group_cols(), everything()) %>%
-    # Unnest results
-    unnest(ends_with("forecast")) %>%
-    ungroup() %>%
-    mutate(index = time_class_fn(index)) %>%
-    mutate(model = str_replace(model, "_model", ""))
+    rename_all(str_replace, "_model_", "_")
+
+  model_count <- colnames(.ts) %>%
+    str_detect("model") %>%
+    sum()
+
+  if (model_count == 1) {
+    forecast_df <- forecast_df %>%
+      select(group_cols(), ends_with("model"), ends_with("forecast")) %>%
+      mutate_at(vars(ends_with("model")), ~1) %>%
+      pivot_longer(ends_with("model"), names_to = "model") %>%
+      select(-value) %>%
+      mutate(model = str_replace(model, "_model", "")) %>%
+      group_by_at(vars(-ends_with("forecast"))) %>%
+      select(group_cols(), everything()) %>%
+      unnest(accuracy) %>%
+      ungroup() %>%
+      mutate(model = str_replace(model, "_model", ""))
+  } else {
+    forecast_df <- forecast_df %>%
+      select(group_cols(), ends_with("forecast")) %>%
+      pivot_longer(ends_with("forecast"), names_to = "model", values_to = "forecast") %>%
+      mutate(model = str_replace(model, "_forecast", "")) %>%
+      group_by_at(vars(-ends_with("forecast"))) %>%
+      select(group_cols(), everything()) %>%
+      unnest(forecast) %>%
+      ungroup() %>%
+      mutate(model = str_replace(model, "_model", ""))
+  }
+
+  if (ts_frequency == 12) {
+    forecast_df <- forecast_df %>%
+      mutate(index = str_c(str_sub(index, 5, 8),
+                           str_sub(index, 1, 3),
+                           sep = " ") %>%
+               time_class_fn())
+  } else {
+    forecast_df <- forecast_df %>%
+      mutate(index = time_class_fn(index))
+  }
+  forecast_df
 }
