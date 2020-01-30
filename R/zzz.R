ccf98207c1c84985e1f05bdca7800716b7da0ac7
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2020-01-30 09:53:19 -0800

    Update .onLoad

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 .onLoad <- function(libname, pkgname) {
-  requireNamespace(forecast)
-  requireNamespace(lubridate)
-  requireNamespace(stringr)
-  requireNamespace(purrr)
+  # requireNamespace(forecast)
+  # requireNamespace(lubridate)
+  # requireNamespace(stringr)
+  # requireNamespace(purrr)
   invisible()
 }
