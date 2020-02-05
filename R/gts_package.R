e4900274302c3ad22215a836b5f2b674b0cd6b68
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2020-02-05 08:55:20 -0600

    Update ts_unnest_fable

--- R/gts_package.R
+++ R/gts_package.R
@@ -1,6 +1,6 @@
 # Suppress R CMD check note
 #' @import data.table
-#' @importFrom data.table as.data.table is.data.table month year
+#' @importFrom data.table as.data.table fifelse is.data.table month year
 #' @importFrom dplyr as_tibble group_split mutate rename
 #' @importFrom purrr map map_lgl
 #' @importFrom rlang abort enexpr enexprs expr eval_tidy sym
--- R/unnest_fable.R
+++ R/unnest_fable.R
@@ -7,22 +7,32 @@
 #' @export
 #'
 #' @examples
-ts_unnest_fable <- function(.data, col) {
+ts_unnest_fable <- function(.data, col, get_sd = FALSE) {
   col <- enexpr(col)
 
   col_class <- eval_tidy(expr(class('$'(.data, !!col)[[1]])))[1]
 
   if (col_class != "fbl_ts") abort("unnest col must be a fable")
 
+  # Keep all columns that are not list columns
   keep_cols <- colnames(.data)[!map_lgl(.data, is.list)] %>%
     syms()
 
-  suppressWarnings(
+  .data <- suppressWarnings(
     .data %>%
       select(!!!keep_cols, !!col) %>%
-      unnest_legacy(!!col) %>%
-      ungroup() %>%
-      mutate(.sd = map_dbl(.distribution, pluck, 2)) %>%
-      select(-.distribution)
+      unnest_legacy(!!col)
   )
+
+  if (get_sd) {
+    .data %>%
+      # If forecast is NA convert .distribution to list of 0s
+      # This allows for extraction of sd without errors
+      mutate(.distribution = fifelse(is.na(count), list(list(0, 0)), .distribution),
+             .sd = map_dbl(.distribution, pluck, 2)) %>%
+      select(-.distribution)
+  } else {
+    .data %>%
+      select(-.distribution)
+  }
 }
